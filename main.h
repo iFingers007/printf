fa9ab324fe0da8169b852e1a67bf3f6666985053
Abu Jehoiada <abujehoiada@gmail.com>
2023-09-17 19:20:35 -0700

    Code update

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/*function prototypes*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(const char *s);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,122 @@
+#include "main.h"
+
+/**
+ * print_int - Handles Integer printing
+ *@list_num: Number to be converted
+ *@n: Pointer to number of characters
+ *
+ *Return: Void
+ */
+
+void print_int(int list_num, int *n)
+{
+	if (list_num < 0)
+	{
+		_putchar('-');
+		list_num = -list_num;
+	}
+	if (list_num == 0)
+		return;
+	print_int((list_num / 10), n);
+	_putchar('0' + (list_num % 10));
+	(*n)++;
+}
+
+/**
+ * print_str - Handles string printing
+ *@s: String to be printed
+ *@n: Pointer to number of characters
+ *
+ *Return: Void
+ */
+void print_str(char *s, int *n)
+{
+	int i = 0;
+
+	if (s == NULL)
+	{
+		write(2, "Error\n", 6);
+		return;
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+		n++;
+	}
+}
+
+/**
+ * print_bin - Handles Binary printing
+ *@list_num: Number to be converted
+ *@n: Number of characters
+ *
+ *Return: Void
+ */
+
+void print_bin(unsigned int list_num, int *n)
+{
+	if (list_num == 0)
+		return;
+	print_bin((list_num / 2), n);
+	_putchar('0' + (list_num % 2));
+	(*n)++;
+}
+
+/**
+ * formatted - Handles formatted strings
+ *@s: String character to be handled
+ *@n: Pointer to number of printed characters
+ *@list: Passed argument
+ *Return: Number of characters printed
+ */
+
+void formatted(char s, va_list list, int *n)
+{
+	if (s == 'c')
+		*n += _putchar(va_arg(list, int));
+	else if (s == 's')
+		print_str(va_arg(list, char *), n);
+	else if (s == 'd' || s == 'i')
+		print_int(va_arg(list, int), n);
+	else if (s == '%')
+		*n += _putchar('%');
+	else if (s == 'b')
+		print_bin(va_arg(list, unsigned int), n);
+	else
+		write(2, "Error", 6);
+
+}
+
+/**
+ * _printf - Custom printf implementation
+ *@format: String format passed as argument
+ *
+ *Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int printed = 0, i;
+	va_list list;
+
+	if (format == NULL)
+		return (0);
+	va_start(list, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			printed++;
+		}
+		else
+		{
+			i++;
+			formatted(format[i], list, &printed);
+		}
+		i++;
+	}
+
+	return (printed);
+}
